14d38477bed1485a0d63e29053e124441448ae38
May Pattaranan <56375291+maysunktong@users.noreply.github.com>
2025-02-21 23:55:16 +0100

    Create mainMenu and header

--- controllers/home.controller.js
+++ controllers/home.controller.js
@@ -1,7 +1,7 @@
 export const renderHome = (req, res) => {
   res.render("pages/index", {
     title: "Home",
-    headerTitle: "Home",
+    headerTitle: "The Crunchy Kale Restaurant",
     pageType: "home",
     favicon: "",
     dataArray: "",
